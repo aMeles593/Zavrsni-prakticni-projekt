23b12ad29f1b8e75dd6f7a92f316e08e67bd89ff
Antonio <antonio.meles44@gmail.com>
2026-09-16 22:14:25 +0200

    Db.js

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -10,7 +10,10 @@ const db = new Pool({
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default db;
